b022626fdbe8dec99eea870d33a19430e712b502
michaelstucki <stcuthbertus@protonmail.com>
2025-11-27 11:49:04 -0700

    initial commit

---
+++ 03-spring-boot-hibernate-jpa-crud/01-cruddemo-student/src/test/java/com/michaelstucki/cruddemo/CruddemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.michaelstucki.cruddemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CruddemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
