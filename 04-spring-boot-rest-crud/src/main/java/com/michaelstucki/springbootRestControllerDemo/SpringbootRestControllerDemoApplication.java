8a490c05626a135b15c23a88bc32a3a7210e781a
michaelstucki <stcuthbertus@protonmail.com>
2025-12-03 16:09:33 -0700

    initial commit of Spring Boot rest controller

---
+++ 04-spring-boot-rest-crud/src/main/java/com/michaelstucki/springbootRestControllerDemo/SpringbootRestControllerDemoApplication.java
@@ -0,0 +1,13 @@
+package com.michaelstucki.springbootRestControllerDemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootRestControllerDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootRestControllerDemoApplication.class, args);
+	}
+
+}
