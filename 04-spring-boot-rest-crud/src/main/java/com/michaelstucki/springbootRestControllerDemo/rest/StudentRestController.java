9a414f3c861ab976b62f6f09f132b7d92d8080c2
michaelstucki <stcuthbertus@protonmail.com>
2025-12-05 15:30:15 -0700

    use AOP to handle controller errors

--- 04-spring-boot-rest-crud/src/main/java/com/michaelstucki/springbootRestControllerDemo/rest/StudentRestController.java
+++ 04-spring-boot-rest-crud/src/main/java/com/michaelstucki/springbootRestControllerDemo/rest/StudentRestController.java
@@ -2,9 +2,10 @@ package com.michaelstucki.springbootRestControllerDemo.rest;
 
 import com.michaelstucki.springbootRestControllerDemo.entity.Student;
 import jakarta.annotation.PostConstruct;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -34,23 +35,5 @@ public class StudentRestController {
             throw new StudentNotFoundException("Student id " + studentId + " not found");
         }
         return students.get(studentId);
-    }
-
-    @ExceptionHandler
-    public ResponseEntity<StudentErrorResponse> handleException(StudentNotFoundException e) {
-        StudentErrorResponse response = new StudentErrorResponse();
-        response.setStatus(HttpStatus.NOT_FOUND.value());
-        response.setMessage(e.getMessage());
-        response.setTimestamp(System.currentTimeMillis());
-        return new ResponseEntity<>(response, HttpStatus.NOT_FOUND);
-    }
-
-    @ExceptionHandler
-    public ResponseEntity<StudentErrorResponse> handleException(Exception e) {
-        StudentErrorResponse response = new StudentErrorResponse();
-        response.setStatus(HttpStatus.BAD_REQUEST.value());
-        response.setMessage(e.getMessage());
-        response.setTimestamp(System.currentTimeMillis());
-        return new ResponseEntity<>(response, HttpStatus.BAD_REQUEST);
     }
 }
---
+++ 04-spring-boot-rest-crud/src/main/java/com/michaelstucki/springbootRestControllerDemo/rest/StudentRestExceptionHandler.java
@@ -0,0 +1,28 @@
+package com.michaelstucki.springbootRestControllerDemo.rest;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
+public class StudentRestExceptionHandler {
+
+    @ExceptionHandler
+    public ResponseEntity<StudentErrorResponse> handleException(StudentNotFoundException e) {
+        StudentErrorResponse response = new StudentErrorResponse();
+        response.setStatus(HttpStatus.NOT_FOUND.value());
+        response.setMessage(e.getMessage());
+        response.setTimestamp(System.currentTimeMillis());
+        return new ResponseEntity<>(response, HttpStatus.NOT_FOUND);
+    }
+
+    @ExceptionHandler
+    public ResponseEntity<StudentErrorResponse> handleException(Exception e) {
+        StudentErrorResponse response = new StudentErrorResponse();
+        response.setStatus(HttpStatus.BAD_REQUEST.value());
+        response.setMessage(e.getMessage());
+        response.setTimestamp(System.currentTimeMillis());
+        return new ResponseEntity<>(response, HttpStatus.BAD_REQUEST);
+    }
+}
